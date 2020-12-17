f56c782f75d157f8148edd69d53dc0bbe0cb0be3
JP Angelle <jp@angelle.me>
2020-12-16 20:28:29 -0600

    add styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,43 @@ import axios from 'axios';
 
 function App() {
   const [price, setPrice] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     (async () => {
-      const { data } = await axios.get('/api/get-kitco');
-      setPrice(data.price);
-      return data;
+      try {
+        const { data } = await axios.get('/api/get-kitco');
+        setPrice(data.price);
+      } catch {
+        setError(true);
+      }
     })();
   }, []);
 
-  return <div className="App">{price}</div>;
+  if (loading) {
+    return (
+      <div className="App">
+        <div class="lds-ripple">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="App">Oh no! Please refresh.</div>;
+  }
+
+  return (
+    <div className="App">
+      <div className="label">Kitco Gold Price</div>
+      <div className="price">{price}</div>
+    </div>
+  );
 }
 
 export default App;
